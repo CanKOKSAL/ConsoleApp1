1b32d830a743c2096c92f328a9201209024d00af
o <o@LAPTOP-4BIGA5QO>
2022-12-08 12:26:29 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿
+//int sayı = Int32.Parse(Console.ReadLine());
+
+//if (sayı<18)
+//{
+//    Console.WriteLine(18-sayı);
+//    Console.WriteLine("sene sonra başvurun");
+//}
+//else if (sayı>=18)
+//{
+
+//    Console.WriteLine("ehliyet için başvurma şartına sahipsiniz");
+
+//}
+
+
+
+
+//int i = 0;
+// for(i=0; i<11; i++)
+//{
+//    Console.WriteLine(i+i);
+
+//}
+
+
+
+
+//int x=Int32.Parse(Console.ReadLine());
+//if (x % 2 == 0)
+//{
+//    Console.WriteLine(x / 2);
+//}
+//else if(x % 2 == 1)
+//{
+//    Console.WriteLine(x*2);
+
+//}
+
+
+
+//double i = 1;
+// for(i=1;i<101;i++)
+//{
+//    Console.WriteLine(i/5);
+
+
+//}
+
+
